dcadf3405c7f2efbbba522037407531781e4f8e7
이주연 <37902200+Yongmalyang@users.noreply.github.com>
2024-09-27 22:07:52 +0900

    주석 추가

--- 2024_E-PPER/Answers/6_Medium_Answer.cpp
+++ 2024_E-PPER/Answers/6_Medium_Answer.cpp
@@ -1,23 +1,41 @@
 #include<vector>
 using namespace std;
 
+// dp 함수. 파라미터: n, m, board
 int dp(int n, int m, vector<vector<int>> &board) {
+    // 2차원 배열 dp, 크기는 n+1 x m+1
     vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    // 정답 answer
     int ans = 0;
+
+    // 반복하면서 찾기!! 1부터 n까지
     for (int i = 1; i <= n; i++) {
+        // 또 반복. 1부터 m까지
         for (int j = 1; j <= m; j++) {
+            // board[i-1][j-1]이 1이면 거기에 놓을 수 있다는 뜻!
             if (board[i - 1][j - 1]) {
+                // i-1, j
+                // i, j-1
+                // i-1, j-1중에 최솟값에 1 더해서 i, j에 넣어주기
                 dp[i][j] = min(min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
             }
+            // ans에 최댓값 갱신
             ans = max(ans, dp[i][j]);
         }
     }
+    // 정답 return
     return ans;
 }
 
+// 솔루션 함수
 int solution(vector<vector<int>> board) {
+    // n은 board의 크기
     int n = board.size();
+    // m은 board[0]의 크기
     int m = board[0].size();
+
+    // dp 돌려서 변수에 넣기
     int square_len = dp(n, m, board);
+    // 해당 변수로 넓이 구하기
     return square_len * square_len;
 }
--- 2024_E-PPER/Answers/7_Medium_Answer.cpp
+++ 2024_E-PPER/Answers/7_Medium_Answer.cpp
@@ -1,39 +1,56 @@
 #include <vector>
 using namespace std;
 
+// 백트래킹 함수 
+// 파라미터: row, n, columns, diag_up, diag_down
 int backtracking(int row, int n, vector<bool>& columns, vector<bool>& diag_up, vector<bool>& diag_down) {
     // n개의 퀸을 모두 배치한 경우
     if (row == n) {
+        // 해답 1개 찾음
         return 1;
     }
 
+    // 정답 0으로 설정
     int answers = 0;
+
+    // n개의 col 뒤지면서 탐색!!
     for (int col = 0; col < n; ++col) {
-        // 퀸을 놓을 수 있으면
+        // 퀸 못 놓으면 지나가기. 
+        // columns[col]: 해당 열에 이미 퀸을 내려놨는가
+        // diag_up[row + col]: 상향 대각선에 퀸이 있는가
+        // diag_down[row - col + n - 1]: 하향 대각선에 퀸이 있는가
         if (columns[col] || diag_up[row + col] || diag_down[row - col + n - 1]) {
             continue;
         }
 
-        // 퀸 배치
+        // continue에 걸리지 않았다면!!
+        // colums[col], diag_up[row+col], diag_down[row-col+n-1] 다 내려두기
         columns[col] = true;
         diag_up[row + col] = true;
         diag_down[row - col + n - 1] = true;
 
+        // 재귀로 호출, row만 +1, answer에다가 호출된 결과
         answers += backtracking(row + 1, n, columns, diag_up, diag_down);
 
-        // 다음 탐색을 위해 퀸 회수
+        // 다시 원상복귀! colums, diag_up, ...
         columns[col] = false;
         diag_up[row + col] = false;
         diag_down[row - col + n - 1] = false;
     }
 
+    // 마지막에 return
     return answers;
 }
 
+// 정답 함수
 int solution(int n) {
-    vector<bool> columns(n, false); // 열 체크
-    vector<bool> diag_up(2 * n - 1, false); // 상향 대각선 체크
-    vector<bool> diag_down(2 * n - 1, false); // 하향 대각선 체크
+    // 열 체크
+    vector<bool> columns(n, false); 
+    // 상향 대각선 체크 (길이 2 이상의 대각선 개수는 2n-1)
+    vector<bool> diag_up(2 * n - 1, false); 
+    // 하향 대각선 체크 (길이 2 이상의 대각선 개수는 2n-1)
+    vector<bool> diag_down(2 * n - 1, false); 
 
+    // 백트래킹 함수 호출 (0번째 row, n, colums, diag_up, diag_down)
     return backtracking(0, n, columns, diag_up, diag_down);
 }
